36657cae356aefe777d148c3dc64c55236f5a45e
boilercoding <boilercoding@gmail.com>
2017-06-29 21:11:00 -0400

    Adds user model

---
+++ web/models/user.ex
@@ -0,0 +1,21 @@
+defmodule Bouncy.User do
+  use Bouncy.Web, :model
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name, :username, :password])
+    |> validate_required([:name, :username, :password])
+  end
+end
